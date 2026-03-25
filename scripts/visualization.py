36de52469d84a7b39f62631f6f1110c951ca8955
Mohitsharma7121 <mohitsharma01127@gmail.com>
2026-03-25 16:49:15 +0530

    visualization.py

--- scripts/visualization.py
+++ scripts/visualization.py
@@ -8,15 +8,45 @@ df = pd.read_csv("data/processed/feature_enriched_dataset.csv")
 
 sample = df[df["commodity"] == df["commodity"].iloc[0]]
 
+# -------------------
+# 1. PRICE TREND
+# -------------------
 plt.figure(figsize=(12,5))
-
 plt.plot(sample["date"], sample["price"])
 plt.title("Price Trend")
 plt.xticks(sample["date"][::20], rotation=45)
-
 plt.tight_layout()
-
-plt.savefig("outputs/plots/trend.png")
+plt.savefig("outputs/plots/price_trend.png")
 plt.close()
 
-print("visualization created")
+
+# -------------------
+# 2. MOVING AVERAGES
+# -------------------
+plt.figure(figsize=(12,5))
+plt.plot(sample["date"], sample["rolling_7"], label="MA 7")
+plt.plot(sample["date"], sample["rolling_14"], label="MA 14")
+plt.plot(sample["date"], sample["rolling_30"], label="MA 30")
+
+plt.title("Moving Averages")
+plt.legend()
+plt.xticks(sample["date"][::20], rotation=45)
+plt.tight_layout()
+plt.savefig("outputs/plots/moving_averages.png")
+plt.close()
+
+
+# -------------------
+# 3. VOLATILITY
+# -------------------
+plt.figure(figsize=(12,5))
+plt.plot(sample["date"], sample["volatility"])
+
+plt.title("Volatility (7-day std)")
+plt.xticks(sample["date"][::20], rotation=45)
+plt.tight_layout()
+plt.savefig("outputs/plots/volatility.png")
+plt.close()
+
+
+print("all visualizations created")
